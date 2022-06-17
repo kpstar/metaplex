16b1c9dfb796a3aaea0e142292a1bf131aa7dab3
Jin <kpstar.mc@gmail.com>
2022-06-17 17:08:02 +0800

    Updates

---
+++ _next/static/mrpLE-G4J__AyZgfyiOpD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-901a189f9a8685b220ab.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
